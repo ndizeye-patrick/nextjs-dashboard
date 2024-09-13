875d4bbbf01d610747a206855f933d3a880df22a
patrick_ndizeye <116964464+ndizeye-patrick@users.noreply.github.com>
2024-09-13 15:54:11 +0200

    finally

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,7 +16,6 @@ export async function fetchRevenue() {
 
     console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     console.log('Data fetch completed after 3 seconds.');
@@ -30,6 +29,8 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    await new Promise((resolve)=> {setTimeout(resolve , 2000)})
+    console.log("data fetched in 200 seconds")
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
